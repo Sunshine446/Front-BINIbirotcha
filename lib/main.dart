b52efc9ee5f099b8570152999fb2338955d88dc7
sunshine446 <sunshinenavarro5@gmail.com>
2024-06-20 12:11:43 +0800

    ACTIVITY 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
-      appBar: AppBar(
-        title: Center(
-          child: Text('BINI Store'),
-        ),
-        backgroundColor: Colors.pink[200],
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    backgroundColor: Colors.white,
+    appBar: AppBar(
+      title: Text('BINI Merch'),
+      backgroundColor: Colors.pink[200],
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Name',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              letterSpacing: 1.0,
+            ),
+          ),
+          Text(
+            'SUNSHINE P. NAVARRO',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'Year',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              letterSpacing: 1.0,
+            ),
+          ),
+          Text(
+            'THIRD',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0
+            ),
+          ),
+          SizedBox(height: 30.0,),
+          Text(
+            'Email',
+            style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+              letterSpacing: 1.0,
+            ),
+          ),
+          Text(
+            'SUNSHINENAVARRO5@GMAIL.COM',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+              fontSize: 25.0,
+            ),
+          ),
+        ],
       ),
-      body: Center(
-        child: Image.network('https://images.squarespace-cdn.com/content/v1/52cc4d12e4b09298835f0ac7/1403667412058-6E8U5M39X93Q1X33JJ6R/ke17ZwdGBToddI8pDm48kExLaNK_kCgjDC3-KReOirNZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpzy6JvjzQ_wegVlOu3onTS2fd6A2ANYrpwanXNDP6AEheb3r8u1y9ieAaQHWJiVsLo/VS_Prototype_1')
-    ), // Center
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: Colors.pink[100],
-        child: Icon(
-          Icons.add,
-          color: Colors.white,
-        ), // Icon
-      ), // FloatingActionButton
-    ), // Scafflod
-  )); // MaterialApp
-}
+    ),
+  ),
+));
