782a76b5e92e2eac4e9edfa095c460524ceab176
Sunshine446 <sunshinenavarro5@gmail.com>
2024-07-08 15:23:38 +0800

    selected product

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:untitled1/pages/signup.dart';
 
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/login',
+  initialRoute: '/menu',
   routes: {
     '/dashboard' : (context) => Dashboard(),
     '/menu' : (context) => Menu(),
--- lib/pages/selectedProduct.dart
+++ lib/pages/selectedProduct.dart
@@ -6,14 +6,24 @@ class selectedProduct extends StatefulWidget {
   const selectedProduct({super.key, required this.product});
 
   @override
-  State<selectedProduct> createState() => _selectedProductState();
+  State<selectedProduct> createState() => _selectedProductState(product: product);
 }
 
 class _selectedProductState extends State<selectedProduct> {
+  final Product product;
+  late double totalAmount;
+  int numberOfOrders = 1;
+  _selectedProductState({required this.product});
+
+  @override
+  void initState() {
+    super.initState();
+    totalAmount = product.price;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text ('Order'),
         backgroundColor: Colors.pink[200],
@@ -25,14 +35,50 @@ class _selectedProductState extends State<selectedProduct> {
         children: [
           Text(widget.product.productName),
           Text(widget.product.description),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+            Text(
+              '₱ ${totalAmount.toString()}',
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+            Row(
+                children:[
+                  IconButton(
+                      onPressed: (){
+                       setState: ((){
+                         if(numberOfOrders > 1){
+                           numberOfOrders -= 1;
+                            totalAmount = product.price * numberOfOrders;
+                        }
+                       });
+                     },
+                     icon: Icon(Icons.remove)
+    ),
+
+            Text(
+              numberOfOrders.toString(),
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+            IconButton(
+              onPressed: (){
+                setState(() {
+                  numberOfOrders += 1;
+                  totalAmount = product.price * numberOfOrders;
+                });
+              },
+              icon: Icon(Icons.add)
+            ),
+                ]
+            ),
+          ]
+      ),
         ],
       ),
-
-      Row(
-        children: [
-          Text(widget.product.price.toString()),
-        ],
-      );
-    );
+    ); //
   }
 }
